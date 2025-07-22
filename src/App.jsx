75c724b4555e7638c0563ac266fd04b895c08ec5
Ankit Shaw <153160423+masterankit26@users.noreply.github.com>
2025-07-22 12:18:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,4 @@
-/*import './App.css'
-import Page1 from './pages/Page1'
-import Page2 from './pages/Page2'
-import Page3 from './pages/Page3'
-import React from 'react'
-import Header from './components/Header'
 
-function App() {
-  
-
-  return (
-    <>
-    <Header />
-    <Page1 />
-    <Page2 />
-    <Page3 />
-    </>
-  )
-}
-
-export default App
-*/
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
