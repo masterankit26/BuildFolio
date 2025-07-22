158fbb61a4b26c6d8452335820492fe9c75b248f
Ankit Shaw <153160423+masterankit26@users.noreply.github.com>
2025-07-22 12:28:18 +0530

    Update Page1ButtomText.jsx

--- src/components/Page1ButtomText.jsx
+++ src/components/Page1ButtomText.jsx
@@ -5,8 +5,8 @@ const Page1ButtomText = () => {
   return (
     <div className='absolute bottom-2 left-6 sm:left-10 px-4 py-2 rounded-md shadow-md'>
       <div className='text-white text-sm sm:text-base leading-relaxed font-semibold text-left'>
-        <h2 className='text-3xl font-serif text-amber-50'><span className='text-blue-300'>Ankit Shaw </span>| MERN Stack |</h2>
-        <h3 className='text-3xl font-serif text-amber-50'>JavaScript | <span className='text-amber-300'>Python |</span></h3>
+        <h2 className='text-3xl font-serif text-amber-50'><span className='text-blue-300'>Ankit Shaw-</span>MERN Stack</h2>
+        <h3 className='text-3xl font-serif text-amber-50'>JavaScript |<span className='text-amber-300'>Python</span></h3>
       </div>
     </div>
   );
