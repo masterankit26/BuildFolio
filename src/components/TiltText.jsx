efb79cbcf8c3fe319b859eb4fb696f993e4b6c79
Ankit Shaw <153160423+masterankit26@users.noreply.github.com>
2025-11-24 09:28:44 +0530

    Update TiltText.jsx

--- src/components/TiltText.jsx
+++ src/components/TiltText.jsx
@@ -4,13 +4,13 @@ import React, { forwardRef } from 'react';
 const TiltText = forwardRef((props, ref) => {
   return (
     <div ref={ref} className='text-right text-white font-serif will-change-transform'>
-      <h1 className='text-[3vw] sm:text-3xl md:text-3.5xl lg:text-4.5xl uppercase font-bold leading-tight'>
+      <h1 className='text-[4vw] sm:text-4xl md:text-4xl lg:text-4xl uppercase font-bold leading-tight'>
         I'm <span className='text-green-200'>Full Stack</span>
       </h1>
-      <h1 className='text-[3vw] sm:text-2xl md:text-2.5xl lg:text-3.5xl uppercase font-semibold'>
+      <h1 className='text-[3.5vw] sm:text-2xl md:text-3xl lg:text-3xl uppercase font-semibold'>
         <span className='text-green-200 font-extrabold'>Web</span> Developer
       </h1>
-      <h1 className='text-[3.5vw] sm:text-2xl md:text-2.5xl lg:text-3.5xl uppercase font-medium'>
+      <h1 className='text-[3.5vw] sm:text-2xl md:text-3xl lg:text-3xl uppercase font-medium'>
         with <span className='text-green-200 font-extrabold'>AI/ML</span>
       </h1>
     </div>
