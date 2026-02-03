de86b50d3d0f5b9fe54c567b74b6f863cec0c7b2
Ankit Shaw <153160423+masterankit26@users.noreply.github.com>
2026-02-03 12:21:54 +0530

    Update Page1ButtomText to reflect new title

--- src/components/Page1ButtomText.jsx
+++ src/components/Page1ButtomText.jsx
@@ -5,7 +5,7 @@ const Page1ButtomText = () => {
   return (
     <div className='absolute bottom-2 left-6 sm:left-10 px-4 py-2 rounded-md shadow-md'>
       <div className='text-white text-sm sm:text-base leading-relaxed font-semibold text-left'>
-        <h2 className='text-3xl font-serif text-green-200'><span className='text-white'>Ankit Shaw </span>| KGEC |</h2>
+        <h2 className='text-3xl font-serif text-green-200'><span className='text-white'>Software Developer | </span>Data Analyst |</h2>
         
       </div>
     </div>
